b5d365859078f1a215ea89b91abac267c656db1d
dustjq1004 <dustjq1005@naver.com>
2023-12-02 16:46:04 +0900

    test : 로또 생성 서비스 테스트 수정

--- src/test/java/lotto/LottoCreateServiceTest.java
+++ src/test/java/lotto/LottoCreateServiceTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import lotto.controller.service.LottoCreateService;
 import lotto.domain.entity.Lotto;
 import lotto.domain.entity.LottoMoney;
-import lotto.domain.generator.LottoGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -42,7 +41,7 @@ class LottoCreateServiceTest {
     )
     @DisplayName("돈을 지불하여 로또번호를 받는다.")
     void purchaseLottoTest(int money, int count) {
-        LottoCreateService lottoCreateService = new LottoCreateService(new LottoGenerator());
+        LottoCreateService lottoCreateService = new LottoCreateService();
         LottoMoney lottoMoney = new LottoMoney(money);
         List<Lotto> lottos = lottoCreateService.createRandomLottoNumbers(lottoMoney);
         Assertions.assertThat(lottos.size()).isEqualTo(count);
