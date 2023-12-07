fea840b7fa1be25d7aabc15a0f76fba4b40beda1
dustjq1004 <dustjq1005@naver.com>
2023-12-07 21:53:46 +0900

    refactor : ExceptionMessage enum 클래스로 변경

--- src/main/java/lotto/validation/ModelValidation.java
+++ src/main/java/lotto/validation/ModelValidation.java
@@ -5,50 +5,46 @@ import static lotto.common.Constants.LOTTO_PRICE;
 import static lotto.common.Constants.MAX_LOTTO_NUMBER;
 import static lotto.common.Constants.MIN_LOTTO_NUMBER;
 import static lotto.common.Constants.REMAINDER_ZERO;
-import static lotto.exception.ExceptionMessage.MESSAGE_DUPLICATE_NUMBER;
-import static lotto.exception.ExceptionMessage.MESSAGE_LIMIT_MONEY;
-import static lotto.exception.ExceptionMessage.MESSAGE_LIMIT_SIZE;
-import static lotto.exception.ExceptionMessage.MESSAGE_RANGE_NUMBER;
-import static lotto.exception.ExceptionMessage.MESSAGE_THOUSAND_UNIT;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import lotto.common.Constants;
 import lotto.exception.ClientException;
+import lotto.exception.ExceptionMessage;
 
 public class ModelValidation {
 
     public static void validateSize(List<Integer> numbers) {
         if (numbers.size() != Constants.LOTTO_SIZE) {
-            throw new ClientException(MESSAGE_LIMIT_SIZE);
+            throw new ClientException(ExceptionMessage.MESSAGE_LIMIT_SIZE.getMessage());
         }
     }
 
     public static void validateDuplicate(List<Integer> numbers) {
         Set<Integer> validationDuplicate = new HashSet<>(numbers);
         if (numbers.size() != validationDuplicate.size()) {
-            throw new ClientException(MESSAGE_DUPLICATE_NUMBER);
+            throw new ClientException(ExceptionMessage.MESSAGE_DUPLICATE_NUMBER.getMessage());
         }
     }
 
     public static void validateRange(List<Integer> numbers) {
         numbers.forEach((number) -> {
             if (number < MIN_LOTTO_NUMBER || MAX_LOTTO_NUMBER < number) {
-                throw new ClientException(MESSAGE_RANGE_NUMBER);
+                throw new ClientException(ExceptionMessage.MESSAGE_RANGE_NUMBER.getMessage());
             }
         });
     }
 
     public static void validateThousandUnit(int money) {
         if (money % LOTTO_PRICE > REMAINDER_ZERO) {
-            throw new ClientException(MESSAGE_THOUSAND_UNIT);
+            throw new ClientException(ExceptionMessage.MESSAGE_THOUSAND_UNIT.getMessage());
         }
     }
 
     public static void validateLimit(int money) {
         if (money > LIMIT_MONEY) {
-            throw new ClientException(MESSAGE_LIMIT_MONEY);
+            throw new ClientException(ExceptionMessage.MESSAGE_LIMIT_MONEY.getMessage());
         }
     }
 }
